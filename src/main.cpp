2e40d9e14ea16782887d2a74175d72a3a3b1673c
Elton Law <eltonlaw296@gmail.com>
2021-01-31 05:07:45 -0500

    use qml over qtwidgets

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
-#include <QApplication>
-#include <QPushButton>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    QPushButton hello("Hello world!");
-    hello.resize(100,30);
-    hello.show();
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
     return app.exec();
 }
