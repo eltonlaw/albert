9f96937b4309204f3808464cb27bb2ea81204fc4
Elton Law <eltonlaw296@gmail.com>
2021-03-18 13:46:35 -0400

    add deps on qtquick libs

--- src/pi-main-controller.mk
+++ src/pi-main-controller.mk
@@ -2,7 +2,7 @@ PI_MAIN_CONTROLLER_VERSION = 0.0.1
 PI_MAIN_CONTROLLER = MIT
 PI_MAIN_CONTROLLER_SITE = $(TOPDIR)/package/pi-main-controller
 PI_MAIN_CONTROLLER_SITE_METHOD = local
-PI_MAIN_CONTROLLER_DEPENDENCIES = qt5base
+PI_MAIN_CONTROLLER_DEPENDENCIES = qt5base qt5quickcontrols qt5quickcontrols2 qt5charts
 
 define PI_MAIN_CONTROLLER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/pi-main-controller $(TARGET_DIR)/usr/bin/pi-main-controller
