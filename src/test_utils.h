2b941bf0f111a04b38c080a8278249bf5cf642fa
Elton Law <eltonlaw296@gmail.com>
2021-04-01 22:30:40 -0400

    [TEST] impl test framework assert_equal

---
+++ src/test_utils.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdlib.h>
+
+namespace testing {
+
+// FIXME: pass through cmake build
+#define ON_FAIL_EXIT 1
+#define ON_FAIL ON_FAIL_EXIT
+
+template <class T>
+void assert_equal(T x, T y) {
+    try {
+        if (x == y) {
+            std::cout << ".";
+        } else {
+            std::cout << "F";
+            if (ON_FAIL == ON_FAIL_EXIT) {
+                std::cout << std::endl << x << " != " << y << std::endl;
+                exit (EXIT_FAILURE);
+            }
+        }
+    } catch (std::exception& e) {
+        std::cout << "E";
+        if (ON_FAIL == ON_FAIL_EXIT) {
+            std::cout << std::endl << x << " != " << y << std::endl;
+            exit (EXIT_FAILURE);
+        }
+    }            
+}
+
+} // namespace testing
