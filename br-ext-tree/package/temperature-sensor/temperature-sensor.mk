27ae955966494fb313d2658411f54d21f476dc59
Elton Law <eltonlaw296@gmail.com>
2021-04-23 01:03:03 -0400

    [TEMPSENSOR] systemd: systemd + dbus conf files

--- br-ext-tree/package/temperature-sensor/temperature-sensor.mk
+++ br-ext-tree/package/temperature-sensor/temperature-sensor.mk
@@ -9,4 +9,18 @@ define TEMPERATURE_SENSOR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/temperature-sensor $(TARGET_DIR)/usr/bin/temperature-sensor
 endef
 
+define TEMPERATURE_SENSOR_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 \
+		$(@D)/temperature-sensor.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/temperature-sensor.service
+endef
+
+ifeq ($(BR2_PACKAGE_DBUS),y)
+define TEMPERATURE_SENSOR_INSTALL_DBUS
+	$(INSTALL) -D -m 644 \
+		$(@D)/org.joomby.TemperatureSensor.conf \
+		$(TARGET_DIR)/etc/dbus-1/system.d/org.joomby.TemperatureSensor.conf
+endef
+endif
+
 $(eval $(cmake-package))
