a35d461f0399f3712b09c3b1cc226091acc10179
Elton Law <eltonlaw296@gmail.com>
2022-11-16 00:29:57 -0500

    fix: add missing albert-temperature-sensor.mk

---
+++ br-ext-tree/package/albert-temperature-sensor/albert-temperature-sensor.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# albert-temperature-sensor
+#
+################################################################################
+
+ALBERT_TEMPERATURE_SENSOR_VERSION = 0.0.1
+ALBERT_TEMPERATURE_SENSOR = MIT
+ALBERT_TEMPERATURE_SENSOR_SITE = $(BR2_EXTERNAL_ALBERT_PATH)/package/albert-temperature-sensor
+ALBERT_TEMPERATURE_SENSOR_SITE_METHOD = local
+ALBERT_TEMPERATURE_SENSOR_DEPENDENCIES = dbus
+
+define ALBERT_TEMPERATURE_SENSOR_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/albert-temperature-sensor $(TARGET_DIR)/usr/bin/albert-temperature-sensor
+endef
+
+$(eval $(cmake-package))
